92d4a93df3a3c9c0b1685ff9009fe92bd8a9b1a0
Andrey Neto <me@andreyneto.com.br>
2019-02-16 16:59:58 -0200

    Update GameAddActivity.kt

--- app/src/main/java/br/com/andreyneto/movilenextday3/GameAddActivity.kt
+++ app/src/main/java/br/com/andreyneto/movilenextday3/GameAddActivity.kt
@@ -1,16 +1,21 @@
 package br.com.andreyneto.movilenextday3
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.content.ContextCompat
 import com.jakewharton.rxbinding2.widget.RxTextView
 import io.reactivex.BackpressureStrategy
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
 import io.reactivex.rxkotlin.Flowables
 import kotlinx.android.synthetic.main.activity_game_add.*
 
 class GameAddActivity : AppCompatActivity() {
 
+    private lateinit var disposable: Disposable
+
+    @SuppressLint("CheckResult")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game_add)
@@ -24,11 +29,11 @@ class GameAddActivity : AppCompatActivity() {
             .skipInitialValue()
             .observeOn(AndroidSchedulers.mainThread())
             .toFlowable(BackpressureStrategy.LATEST)
-        Flowables.combineLatest(nameChangeObservable, yearChangeObservable) {
+        disposable = Flowables.combineLatest(nameChangeObservable, yearChangeObservable) {
                 newName: CharSequence, newYear: CharSequence ->
             val nameValid = newName.length > 4
             if(!nameValid) etName.error = "Invalid name"
-            val yearValid = newName.length > 4
+            val yearValid = newName.length == 4
             if(!yearValid) etYear.error = "Invalid year"
             nameValid && yearValid
         }.subscribe {formValid->
@@ -36,5 +41,10 @@ class GameAddActivity : AppCompatActivity() {
                 ContextCompat.getColor(applicationContext,
                     if (formValid) R.color.colorPrimaryDark else android.R.color.darker_gray))
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        disposable.dispose()
     }
 }
