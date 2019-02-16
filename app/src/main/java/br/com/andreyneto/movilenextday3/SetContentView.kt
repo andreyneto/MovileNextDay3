969e61b1bbf89862226f4df7ecbeeb2238602162
Andrey Neto <me@andreyneto.com.br>
2019-02-16 10:56:14 -0200

    Update SetContentView.kt

--- app/src/main/java/br/com/andreyneto/movilenextday3/SetContentView.kt
+++ app/src/main/java/br/com/andreyneto/movilenextday3/SetContentView.kt
@@ -6,11 +6,18 @@ import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import kotlin.reflect.KProperty
 
+fun <T : ViewDataBinding> contentView(
+    @LayoutRes layoutRes: Int
+) = SetContentView<T>(layoutRes)
+
 class SetContentView<out T : ViewDataBinding>(
     @LayoutRes
     private val layoutRes: Int
 ) {
+    private var value : T? = null
+
     operator fun getValue(thisRef: Activity, property: KProperty<*>): T {
-        return  DataBindingUtil.setContentView(thisRef, layoutRes)
+        value = value ?:   DataBindingUtil.setContentView(thisRef, layoutRes)
+        return value!!
     }
 }
