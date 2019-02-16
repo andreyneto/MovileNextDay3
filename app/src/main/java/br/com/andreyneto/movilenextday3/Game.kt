9e6628e42fc3fe4a425d4936aaa3ac938bc9d26c
Andrey Neto <me@andreyneto.com.br>
2019-02-16 11:46:28 -0200

    Update Game.kt

--- app/src/main/java/br/com/andreyneto/movilenextday3/Game.kt
+++ app/src/main/java/br/com/andreyneto/movilenextday3/Game.kt
@@ -2,4 +2,6 @@ package br.com.andreyneto.movilenextday3
 
 data class Game(val name: String,
                 val launchYear: Int,
-                val imageUrl: String)
+                val imageUrl: String) {
+    val isClassic = launchYear < 2000
+}
