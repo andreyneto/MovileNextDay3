4f77fa45a4efbdec020f91ea6cb028721b852b84
Andrey Neto <me@andreyneto.com.br>
2019-02-16 11:57:18 -0200

    Update Game.kt

--- app/src/main/java/br/com/andreyneto/movilenextday3/Game.kt
+++ app/src/main/java/br/com/andreyneto/movilenextday3/Game.kt
@@ -1,7 +1,20 @@
 package br.com.andreyneto.movilenextday3
 
-data class Game(val name: String,
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+
+class Game(val name: String,
                 val launchYear: Int,
-                val imageUrl: String) {
+                val imageUrl: String,
+                rating: Double) : BaseObservable() {
     val isClassic = launchYear < 2000
+
+    var rating = rating
+        @Bindable get
+        set(value) {
+            if(field!=value) {
+                field = value
+                notifyPropertyChanged(BR.rating)
+            }
+        }
 }
