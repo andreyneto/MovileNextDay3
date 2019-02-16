5e1fd915683140a9bc58f282f6108866d1cd3b46
Andrey Neto <me@andreyneto.com.br>
2019-02-16 11:32:08 -0200

    Update MainActivity.kt

--- app/src/main/java/br/com/andreyneto/movilenextday3/MainActivity.kt
+++ app/src/main/java/br/com/andreyneto/movilenextday3/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
-        val game = Game("portal 2", 200, "")
+        val game = Game("portal 2", 200, "https://reclaimer105.files.wordpress.com/2012/01/portal-2-e3-glados.png?w=490&h=283")
         binding.game = game
     }
 }
