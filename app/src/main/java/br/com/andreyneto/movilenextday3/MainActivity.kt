52caf43f475529742b29afb2d8eadc437989abcf
Andrey Neto <me@andreyneto.com.br>
2019-02-16 11:57:21 -0200

    Update MainActivity.kt

--- app/src/main/java/br/com/andreyneto/movilenextday3/MainActivity.kt
+++ app/src/main/java/br/com/andreyneto/movilenextday3/MainActivity.kt
@@ -3,6 +3,7 @@ package br.com.andreyneto.movilenextday3
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.com.andreyneto.movilenextday3.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,7 +12,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
-        val game = Game("portal 2", 200, "https://reclaimer105.files.wordpress.com/2012/01/portal-2-e3-glados.png?w=490&h=283")
+        val game = Game("portal 2",
+            2004,
+            "https://reclaimer105.files.wordpress.com/2012/01/portal-2-e3-glados.png?w=490&h=283",
+            5.5)
         binding.game = game
+        tvRating.setOnClickListener {
+            game.rating += 1
+        }
     }
 }
