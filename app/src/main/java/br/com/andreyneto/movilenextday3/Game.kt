d28df24c0108b9b6dfc74355d3b54b86f4e7014a
Andrey Neto <me@andreyneto.com.br>
2019-02-16 11:13:52 -0200

    Update Game.kt

--- app/src/main/java/br/com/andreyneto/movilenextday3/Game.kt
+++ app/src/main/java/br/com/andreyneto/movilenextday3/Game.kt
@@ -1,3 +1,5 @@
 package br.com.andreyneto.movilenextday3
 
-data class Game(val name: String)
+data class Game(val name: String,
+                val launchYear: Int,
+                val imageUrl: String)
